e086c5ae04f8de31d282858df08f8b7ca631cc8c
kedhaxy <105529817+kedhaxy@users.noreply.github.com>
2022-05-23 23:07:11 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -1,6 +1,7 @@
 install -m 755 /apt/v2ray /usr/local/bin/v2ray
 install -m 755 /apt/v2ctl /usr/local/bin/v2ctl
 install -d /usr/local/etc/v2ray
+chmod +x /apt/heroku.json
 /usr/local/bin/v2ctl config /apt/heroku.json > /usr/local/etc/v2ray/config.pb
 rm -rf /apt
 mkdir /opt/test
