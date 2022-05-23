1ba7920e2edd003aa875a8e547f7faec207e494c
kedhaxy <105529817+kedhaxy@users.noreply.github.com>
2022-05-23 20:26:17 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -1,4 +1,3 @@
-config_path=$PROTOCOL"_ws_tls.json"
 install -m 755 /apt/v2ray /usr/local/bin/v2ray
 install -m 755 /apt/v2ctl /usr/local/bin/v2ctl
 install -d /usr/local/etc/v2ray
@@ -7,5 +6,5 @@ mkdir /opt/test
 cd /opt/test
 dd if=/dev/zero of=100mb.bin bs=100M count=1
 dd if=/dev/zero of=10mb.bin bs=10M count=1
-/usr/local/bin/v2ray -config /usr/local/etc/v2ray/config.pb &
+/usr/local/bin/v2ray -config /apt/config.json &
 nginx -g 'daemon off;'
