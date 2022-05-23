7e7e694b8d75cbb63ae86adcec4b5d671056577b
kedhaxy <105529817+kedhaxy@users.noreply.github.com>
2022-05-23 18:42:25 +0800

    Add files via upload

---
+++ apt/app.sh
@@ -0,0 +1,30 @@
+echo '{
+{
+  "inbounds": [
+    {
+      "port": 23323,
+      "protocol": "vmess",
+      "settings": {
+        "clients": [
+          {
+            "id": "54f87cfd-6c03-45ef-bb3d-9fdacec80a9a",
+            "alterId": 0
+          }
+        ],
+        "disableInsecureEncryption": true
+      },
+      "streamSettings": {
+        "network": "ws",
+        "wsSettings": {
+          "path": "/app"
+        }
+      }
+    }
+  ],
+  "outbounds": [
+    {
+      "protocol": "freedom"
+    }
+  ]
+}
+}' > config.json
