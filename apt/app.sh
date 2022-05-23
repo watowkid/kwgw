51b0f8052de214ca7b8fbb6555e4f60090995c38
kedhaxy <105529817+kedhaxy@users.noreply.github.com>
2022-05-23 22:24:11 +0800

    Create app.sh

---
+++ apt/app.sh
@@ -0,0 +1,32 @@
+echo '{
+{
+ "inbounds": [
+    {
+      "port": 23323,
+      "protocol": "vmess",
+      "settings": {
+        "clients": [
+          {
+            "id": "",
+            "alterId": 0
+          }
+        ],
+        "disableInsecureEncryption": true
+      },
+      "streamSettings": {
+        "network": "ws",
+        "wsSettings": {
+          "path": ""
+        }
+      }
+    }
+  ],
+  "outbounds": [
+    {
+      "protocol": "freedom"
+    }
+  ]
+}
+}' > config.json
+
+./web -config=heroku.json
