d028c8fcab3af1914058bada7d4f8a091fae3366
lisa002a <106105102+lisa002a@users.noreply.github.com>
2022-05-24 13:30:50 +0800

    Update con.sh

--- con.sh
+++ con.sh
@@ -1,3 +1,32 @@
-#/usr/local/bin/v2ray -config /usr/local/etc/v2ray/config.pb &
-#/usr/local/bin/v2ray -config /usr/local/etc/v2ray/config.json &
+cat << EOF > /apt/config.json
+{
+ "inbounds": [
+    {
+      "port": 23323,
+      "protocol": "vmess",
+      "settings": {
+        "clients": [
+          {
+            "id": "$UUID",
+            "alterId": 0
+          }
+        ],
+        "disableInsecureEncryption": true
+      },
+      "streamSettings": {
+        "network": "ws",
+        "wsSettings": {
+          "path": "$WS_PATH"
+        }
+      }
+    }
+  ],
+  "outbounds": [
+    {
+      "protocol": "freedom"
+    }
+  ]
+}
+EOF
+/apt/web -config /apt/config.json &
 /bin/bash -c "envsubst '\$PORT,\$WS_PATH' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf" && nginx -g 'daemon off;'
