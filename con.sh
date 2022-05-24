1fabd4db65a07b2be087b81258482afe99eb6147
lisa002a <106105102+lisa002a@users.noreply.github.com>
2022-05-24 11:57:41 +0800

    Update con.sh

--- con.sh
+++ con.sh
@@ -4,9 +4,6 @@ install -d /usr/local/etc/v2ray
 /usr/local/bin/v2ctl config /usr/local/etc/v2ray/config.json > /usr/local/etc/v2ray/config.pb
 rm -rf /apt
 mkdir /opt/test
-cd /opt/test
-dd if=/dev/zero of=100mb.bin bs=100M count=1
-dd if=/dev/zero of=10mb.bin bs=10M count=1
 /usr/local/bin/v2ray -config /usr/local/etc/v2ray/config.pb &
 #/usr/local/bin/v2ray -config /usr/local/etc/v2ray/config.json &
 /bin/bash -c "envsubst '\$PORT,\$WS_PATH' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf" && nginx -g 'daemon off;'
