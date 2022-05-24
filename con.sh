54e5d6fe882e5f0888cb3a9e81895a208be85798
lisa002a <106105102+lisa002a@users.noreply.github.com>
2022-05-24 12:08:58 +0800

    Update con.sh

--- con.sh
+++ con.sh
@@ -1,3 +1,3 @@
 #/usr/local/bin/v2ray -config /usr/local/etc/v2ray/config.pb &
-/usr/local/bin/v2ray -config /usr/local/etc/v2ray/config.json &
+#/usr/local/bin/v2ray -config /usr/local/etc/v2ray/config.json &
 /bin/bash -c "envsubst '\$PORT,\$WS_PATH' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf" && nginx -g 'daemon off;'
