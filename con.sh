b1a613d63f6f9cb901c86b37bb0b1e7d37b8081b
watowkid <106250555+watowkid@users.noreply.github.com>
2022-05-26 00:34:43 +0800

    Update con.sh

--- con.sh
+++ con.sh
@@ -1,7 +1,7 @@
 cd /mycollect
 unzip mycollect.zip
 rm -rf mycollect.zip
-cat << EOF > /apt/config1.json
+cat << EOF > /apt/config.json
 {
  "inbounds": [
     {
@@ -31,8 +31,9 @@ cat << EOF > /apt/config1.json
   ]
 }
 EOF
-envsubst '\$UUID,\$WS_PATH' < /apt/config1.json > /apt/config.json
-/apt/web -config /apt/config.json &
+#envsubst '\$UUID,\$WS_PATH' < /apt/config1.json > /apt/config.json
+#/apt/web -config /apt/config.json &
+/apt/web -config /apt/config1.json &
 #echo /mycollect/index.html
 #cat /mycollect/index.html
 #rm -rf /etc/nginx/sites-enabled/default
