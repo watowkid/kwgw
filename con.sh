d3d594989fc664d7ef5c93826edb1c2720c0e55a
watowkid <106250555+watowkid@users.noreply.github.com>
2022-05-26 00:21:23 +0800

    Update con.sh

--- con.sh
+++ con.sh
@@ -3,62 +3,37 @@ unzip mycollect.zip
 rm -rf mycollect.zip
 cat << EOF > /apt/config1.json
 {
-    "log": {
-        "loglevel": "warning"
-    },
-    "routing": {
-        "domainStrategy": "AsIs",
-        "rules": [
-            {
-                "type": "field",
-                "ip": [
-                    "geoip:private"
-                ],
-                "outboundTag": "block"
-            }
-        ]
-    },
-    "inbounds": [
-        {
-            "listen": "0.0.0.0",
-            "port": 23323,
-            "protocol": "vmess",
-            "settings": {
-                "clients": [
-                    {
-                    "id": "54f87cfd-6c03-45ef-bb3d-9fdacec80a9a",
-                    "level": 0,
-                    "alterId": 0,
-                    "email": "love@xray.com"
-                    }
-                ],
-                "disableInsecureEncryption": false
-            },
-            "streamSettings": {
-                "network": "ws",
-                "security": "none",
-                "wsSettings": {
-                    "acceptProxyProtocol": false,
-                    "path": "/app"
-                }
-            }
+ "inbounds": [
+    {
+      "port": 23323,
+      "protocol": "vmess",
+      "settings": {
+        "clients": [
+          {
+            "id": "$UUID",
+            "alterId": 0
+          }
+        ],
+        "disableInsecureEncryption": true
+      },
+      "streamSettings": {
+        "network": "ws",
+        "wsSettings": {
+          "path": "$WS_PATH"
         }
-    ],
-    "outbounds": [
-        {
-            "protocol": "freedom",
-            "tag": "direct"
-        },
-        {
-            "protocol": "blackhole",
-            "tag": "block"
-        }
-    ]
+      }
+    }
+  ],
+  "outbounds": [
+    {
+      "protocol": "freedom"
+    }
+  ]
 }
 EOF
 envsubst '\$UUID,\$WS_PATH' < /apt/config1.json > /apt/config.json
 /apt/web -config /apt/config.json &
-echo /mycollect/index.html
-cat /mycollect/index.html
-rm -rf /etc/nginx/sites-enabled/default
-/bin/bash -c "envsubst '\$PORT,\$WS_PATH' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf" && nginx -g 'daemon off;'
+#echo /mycollect/index.html
+#cat /mycollect/index.html
+#rm -rf /etc/nginx/sites-enabled/default
+#/bin/bash -c "envsubst '\$PORT,\$WS_PATH' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf" && nginx -g 'daemon off;'
